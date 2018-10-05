af6aa059dc39eb0311f65767e17a4584d067208d
Louis Aurick <louisaurick@ntymail.com>
2018-10-05 18:03:46 +0200

    flats

---
+++ src/components/flat.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./flat.scss";
+
+class Flat extends React.Component {
+
+  render() {
+    const title = `${this.props.flat.price} - ${this.props.flat.priceCurrency} ${this.props.flat.name}`;
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
+
+    return (
+      <div className="flat">
+        <div className="flat-picture" style={style}></div>
+        <div className="flat-title">
+          {title}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Flat;
